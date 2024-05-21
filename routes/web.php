60c63bf8921e12235b4a2a70987a0b0a22dd92dd
nakano1107 <basketlag@icloud.com>
2024-05-21 21:28:41 +0900

    カリキュラム08-2_課題2まで

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,10 @@ use App\Http\Controllers\PostController;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/', [PostController::class, 'index']);
+?>
